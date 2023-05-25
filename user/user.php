cb09aaacce13d15c6382032bddc387b7b169e785
Jose Angel Valadez Barajas <skullerushersk1@gmail.com>
2023-05-25 01:26:31 -0600

    Added PHP Login

--- user/user.php
+++ user/user.php
@@ -6,7 +6,9 @@
     $password = "SkullerA.117";
     include 'AtArcadierTool_class.php';
     $sdk = new AtArcadierTool(); 
-    $user = $sdk->subGetAdminToken(); 
-    header("Location: https://towaretail.sandbox.arcadier.io/user/plugins/0db2bace-59df-4070-ad98-d0e2821b8851/user.html", TRUE, 301);
-    exit();
+    list ($user,$id) = $sdk->subGetAdminToken(); 
+    echo $user;
+    echo $id;
+    //header("Location: https://towaretail.sandbox.arcadier.io/user/plugins/0db2bace-59df-4070-ad98-d0e2821b8851/user.html", TRUE, 301);
+    //exit();
 ?>
