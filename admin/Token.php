5cb7599e81d7f2a28f7b8b15850505fbb12ce74f
Jose Angel Valadez Barajas <skullerushersk1@gmail.com>
2023-06-03 12:10:39 -0600

    php mods

--- admin/Token.php
+++ admin/Token.php
@@ -1,11 +1,7 @@
 <?php
     include 'AtArcadierTool_class.php';
     $sdk = new AtArcadierTool();
-    try{
-        [$adminToken,$userID] = $sdk->obtainAdmintoken();
-        $userInfo = array("Token"=>$adminToken, "Id"=>$userID);
-        echo json_encode($userInfo);
-    }catch(Exception $e){
-        return $e->getMessage();
-    }
+    [$adminToken,$userID] = $sdk->obtainAdmintoken();
+    $userInfo = array("Token"=>$adminToken, "Id"=>$userID);
+    echo json_encode($userInfo);
 ?>
